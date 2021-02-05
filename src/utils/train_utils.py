48e261d447e13b257e244282de2ad6e520b2ceb2
RayWilliam46 <70192440+RayWilliam46@users.noreply.github.com>
2021-02-05 11:27:58 -0600

    Update batch_encode function

--- src/utils/train_utils.py
+++ src/utils/train_utils.py
@@ -1,29 +1,36 @@
-def batch_encode(tokenizer, texts):
+def batch_encode(tokenizer, texts, batch_size=256, max_length=params['MAX_LENGTH']):
     """""""""
     A function that encodes a batch of texts and returns the texts'
     corresponding encodings and attention masks that are ready to be fed 
     into a pre-trained transformer model.
-
-
+    
     Input:
-        - tokenizer:  tokenizer object from the PreTrainedTokenizer Class
-        - texts:  a list of strings where each string represents a piece of text
+        - tokenizer:   Tokenizer object from the PreTrainedTokenizer Class
+        - texts:       List of strings where each string represents a text
+        - batch_size:  Integer controlling number of texts in a batch
+        - max_length:  Integer controlling max number of words to tokenize in a given text
     Output:
-        - input_ids:       a text encoded as a tf.Tensor object
-        - attention_mask:  the text's attention mask encoded as a tf.Tensor object
+        - input_ids:       sequence of texts encoded as a tf.Tensor object
+        - attention_mask:  the texts' attention mask encoded as a tf.Tensor object
     """""""""
-    inputs = tokenizer.batch_encode_plus(texts,
-                                         max_length=params['MAX_LENGTH'],
-                                         padding='longest',  # implements dynamic padding
-                                         truncation=True,
-                                         return_tensors='tf',
-                                         return_attention_mask=True,
-                                         return_token_type_ids=False)
-
-    input_ids = inputs['input_ids']
-    attention_mask = inputs['attention_mask']
-
-    return input_ids, attention_mask
+    
+    input_ids = []
+    attention_mask = []
+    
+    for i in range(0, len(texts), batch_size):
+        batch = texts[i:i+batch_size]
+        inputs = tokenizer.batch_encode_plus(batch,
+                                             max_length=max_length,
+                                             padding='longest', #implements dynamic padding
+                                             truncation=True,
+                                             return_attention_mask=True,
+                                             return_token_type_ids=False
+                                             )
+        input_ids.extend(inputs['input_ids'])
+        attention_mask.extend(inputs['attention_mask'])
+    
+    
+    return tf.convert_to_tensor(input_ids), tf.convert_to_tensor(attention_mask)
 
 
 def focal_loss(gamma=params['FL_GAMMA'], alpha=params['FL_ALPHA']):
@@ -32,6 +39,7 @@ def focal_loss(gamma=params['FL_GAMMA'], alpha=params['FL_ALPHA']):
     
     Code adapted from https://gist.github.com/mkocabas/62dcd2f14ad21f3b25eac2d39ec2cc95
     """""""""
+    
     def focal_loss_fixed(y_true, y_pred):
         pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
         pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
